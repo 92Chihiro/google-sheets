812ccb10b0f1905992d06b7b85b72bf62141875d
92Chihiro <34203596+92Chihiro@users.noreply.github.com>
2018-09-20 20:46:48 +0900

    update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('SPREADSHEET_ID', '11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM');
 define('RANGE', 'sales!A1:E6');
 
 $client = new Google_Client();
-$client->setDeveloperKey("AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA");
+$client->setDeveloperKey("");
 
 $service = new Google_Service_Sheets($client);
 $response = $service->spreadsheets_values->get(SPREADSHEET_ID, RANGE);
